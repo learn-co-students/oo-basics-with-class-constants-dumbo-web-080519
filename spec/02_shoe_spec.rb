8d20e09ca8d8edd37f18fb7a2c0421cf12045c4a
Brian Wong <bwong92@gmail.com>
2019-08-07 13:21:15 +0000

    Done.

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -1,4 +1,4 @@
-describe 'Shoe' do 
+describe 'Shoe' do
   describe 'BRANDS' do
     # Think about it! When does the BRANDS constant need to be
     # made aware of the brands of the shoes that are being created?
@@ -25,3 +25,16 @@ describe 'Shoe' do
     end
   end
 end
+# describe 'GENRES' do
+#    it 'keeps track of all genres' do
+#      genres = ["Thriller", "Science Fiction", "Romance"]
+#      genres.each_with_index do |genre, i|
+#        book = Book.new("Book_#{i}")
+#        book.genre = genre
+#      end
+#
+#      genres.each do |genre|
+#        expect(Book::GENRES).to include(genre)
+#      end
+#    end
+#  end
