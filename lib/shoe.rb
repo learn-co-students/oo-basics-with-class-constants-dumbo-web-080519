9014fe32c780a244d330b8e4a2b93bd66c954287
MichelleNguy <plasticwatering@gmail.com>
2019-08-07 17:41:23 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
+require "pry"
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+  attr_reader :brands
+
+  BRANDS = []
 
   def initialize(brand)
-    @brand = brand
+    BRANDS << brand if !BRANDS.include?(brand)
+    self.brand = brand
   end
 
   def cobble
@@ -11,4 +15,22 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
